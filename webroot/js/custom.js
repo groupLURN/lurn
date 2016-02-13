8f4b5872c4009f749110f0dc4363a9d74800d24a
Emmett Young <emmettyoung92@gmail.com>
2016-02-13 18:15:36 +0800

    Enabled pre-assignment of date in datepicker using javascript.

--- webroot/js/custom.js
+++ webroot/js/custom.js
@@ -1,10 +1,14 @@
-$(".datetime-picker").datepicker().datepicker("option","dateFormat", "yy-mm-dd").datepicker("setDate", new Date());
+Date.prototype.isValid = function () {
+    // An invalid date object returns NaN for getTime() and NaN is the only
+    // object not strictly equal to itself.
+    return this.getTime() === this.getTime();
+};
 
-var date_from = '<?= isset($date_from)? $date_from: ""?>';
-if(date_from)
-    $('input.datetime-picker[name=date_from]').val(date_from);
-
-var date_to = '<?= isset($date_to)? $date_to: ""?>';
-if(date_to)
-    $('input.datetime-picker[name=date_to]').val(date_to);
-
+$(".datetime-picker").each(function(){
+    var initialValue = new Date($(this).val());
+    $(this).datepicker().datepicker("option","dateFormat", "yy-mm-dd");
+    if(initialValue.isValid())
+        $(this).datepicker("setDate", initialValue);
+    else
+        $(this).datepicker("setDate", new Date());
+});

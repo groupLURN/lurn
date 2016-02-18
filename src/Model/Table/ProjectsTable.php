3a79c6c0c811b2214f2d5a26b4e686dfb86953e8
Emmett Young <emmettyoung92@gmail.com>
2016-02-18 13:26:37 +0800

    Bake: Projects

---
+++ src/Model/Table/ProjectsTable.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Project;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Projects Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $Clients
+ * @property \Cake\ORM\Association\BelongsTo $Employees
+ * @property \Cake\ORM\Association\BelongsTo $ProjectStatuses
+ */
+class ProjectsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('projects');
+        $this->displayField('title');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Clients', [
+            'foreignKey' => 'client_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Employees', [
+            'foreignKey' => 'project_manager_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('ProjectStatuses', [
+            'foreignKey' => 'project_status_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('title', 'create')
+            ->notEmpty('title');
+
+        $validator
+            ->requirePresence('description', 'create')
+            ->notEmpty('description');
+
+        $validator
+            ->dateTime('start_date')
+            ->requirePresence('start_date', 'create')
+            ->notEmpty('start_date');
+
+        $validator
+            ->dateTime('end_date')
+            ->requirePresence('end_date', 'create')
+            ->notEmpty('end_date');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['client_id'], 'Clients'));
+        $rules->add($rules->existsIn(['project_manager_id'], 'Employees'));
+        $rules->add($rules->existsIn(['project_status_id'], 'ProjectStatuses'));
+        return $rules;
+    }
+}
---
+++ src/Template/Projects/edit.ctp
@@ -0,0 +1,35 @@
+<nav class="large-3 medium-4 columns" id="actions-sidebar">
+    <ul class="side-nav">
+        <li class="heading"><?= __('Actions') ?></li>
+        <li><?= $this->Form->postLink(
+                __('Delete'),
+                ['action' => 'delete', $project->id],
+                ['confirm' => __('Are you sure you want to delete # {0}?', $project->id)]
+            )
+        ?></li>
+        <li><?= $this->Html->link(__('List Projects'), ['action' => 'index']) ?></li>
+        <li><?= $this->Html->link(__('List Clients'), ['controller' => 'Clients', 'action' => 'index']) ?></li>
+        <li><?= $this->Html->link(__('New Client'), ['controller' => 'Clients', 'action' => 'add']) ?></li>
+        <li><?= $this->Html->link(__('List Employees'), ['controller' => 'Employees', 'action' => 'index']) ?></li>
+        <li><?= $this->Html->link(__('New Employee'), ['controller' => 'Employees', 'action' => 'add']) ?></li>
+        <li><?= $this->Html->link(__('List Project Statuses'), ['controller' => 'ProjectStatuses', 'action' => 'index']) ?></li>
+        <li><?= $this->Html->link(__('New Project Status'), ['controller' => 'ProjectStatuses', 'action' => 'add']) ?></li>
+    </ul>
+</nav>
+<div class="projects form large-9 medium-8 columns content">
+    <?= $this->Form->create($project) ?>
+    <fieldset>
+        <legend><?= __('Edit Project') ?></legend>
+        <?php
+            echo $this->Form->input('client_id', ['options' => $clients]);
+            echo $this->Form->input('project_manager_id', ['options' => $employees]);
+            echo $this->Form->input('project_status_id', ['options' => $projectStatuses]);
+            echo $this->Form->input('title');
+            echo $this->Form->input('description');
+            echo $this->Form->input('start_date');
+            echo $this->Form->input('end_date');
+        ?>
+    </fieldset>
+    <?= $this->Form->button(__('Submit')) ?>
+    <?= $this->Form->end() ?>
+</div>

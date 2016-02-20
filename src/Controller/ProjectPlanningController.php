3a185834d497a2d4468e454b73d9270e53a34352
Emmett Young <emmettyoung92@gmail.com>
2016-02-20 18:34:34 +0800

    Created Project Planning Controller
    - Created action for createGanttChart
    - Initialized the gantt chart.

---
+++ src/Controller/ProjectPlanningController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * ProjectPlanning Controller
+ *
+ */
+class ProjectPlanningController extends ProjectOverviewController
+{
+
+    /**
+     * Index method
+     *
+     * @return void
+     */
+    public function index()
+    {
+    }
+
+    public function createGanttChart()
+    {
+    }
+
+}
---
+++ webroot/js/gantt-chart.js
@@ -0,0 +1,1 @@
+gantt.init("gantt-chart");

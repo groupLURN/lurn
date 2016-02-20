c391642ec2dd6e44d872e8bbf15c68f6a4e6f10c
Emmett Young <emmettyoung92@gmail.com>
2016-02-21 00:48:37 +0800

    Automatically redirect to dashboard if no project is selected.

--- src/Controller/ProjectOverviewController.php
+++ src/Controller/ProjectOverviewController.php
@@ -13,6 +13,9 @@ class ProjectOverviewController extends AppController
 
     public function beforeFilter(Event $event)
     {
+        if(empty($this->request->params['pass']))
+            return $this->redirect(['controller' => 'dashboard']);
+        
         $this->viewBuilder()->layout('project_management');
         $this->set('project_id', $this->request->params['pass'][0]);
         return parent::beforeFilter($event);

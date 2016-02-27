9f52af0961c6704b8ae02ba475f3e6b388ca4b51
Xegara <emmettyoung92@gmail.com>
2016-02-27 17:21:45 +0800

    New Migration: Equipment General Inventory

---
+++ config/Migrations/20160227091941_CreateEquipmentGeneralInventory.php
@@ -0,0 +1,38 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateEquipmentGeneralInventory extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('equipment_general_inventories');
+        $table->addColumn('equipment_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('quantity', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+
+        $table->addForeignKey('equipment_id', 'equipment');
+        $table->create();
+    }
+}

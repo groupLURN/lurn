4b3fd9b4ab69a848eeaf04200d41ccefc0791a51
bigbau <julius_bautista@rocketmail.com>
2016-11-02 11:52:11 +0800

    New way of adding links to the database,

--- src/Controller/ResourceRequestHeadersController.php
+++ src/Controller/ResourceRequestHeadersController.php
@@ -84,7 +84,8 @@ class ResourceRequestHeadersController extends AppController
 
                 foreach ($employees as $employee) {
                     $notification = $this->Notifications->newEntity();
-                    $notification->link = Router::url(['controller' => 'resource-request-headers', 'action' => 'view/'. $project->id ], false);
+                    $link =  str_replace(Router::url('/', false), "", Router::url(['controller' => 'resource-request-headers', 'action' => 'view/'. $project->id ], false));
+                    $notification->link = $link;
                     $notification->message = $project->title.' has made an inventory request. Click to see request.';
                     $notification->user_id = $employee['user_id'];
                     $notification->project_id = $resourceRequestHeader->from_project_id;

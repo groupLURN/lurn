a77ba773d17033a7caf3c593bca350a1cf8ef8e4
bigbau <julius_bautista@rocketmail.com>
2017-01-30 17:07:44 +0800

    Added migrations for the new fields of employee and manpower

---
+++ config/Migrations/20170130090031_AddAgeAddressContactToEmployee.php
@@ -0,0 +1,33 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddAgeAddressContactToEmployee extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('employees');
+		
+        $table->addColumn('address', 'string', [
+            'default' => null,
+            'null' => true,
+        ]);
+		
+        $table->addColumn('age', 'int', [
+            'default' => 0,
+            'null' => true,
+        ]);
+		
+        $table->addColumn('contact-number', 'string', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->update();
+    }
+}
